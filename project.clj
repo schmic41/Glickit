469e9917fd133b5c6d81d37b3a50c366cfe02710
cameron <schmic41@gmail.com>
2018-04-16 01:24:19 -0700

    Added url and changed version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject glickit "0.1.0"
+(defproject glickit "1.0.0"
   :description "A Glicko-2 implementation written in Clojure."
   :license {:name "MIT License"}
+  :url "https://github.com/schmic41/Glickit"
   :dependencies [[org.clojure/clojure "1.9.0"]])
